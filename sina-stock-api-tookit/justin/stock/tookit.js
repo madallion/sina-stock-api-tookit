5ce881301fde740136e61c479b655582db787c7e
pluswolfboy@gmail.com <pluswolfboy@gmail.com@0f21b161-1307-2799-8ae5-f39eb8b2c6e2>
2011-03-14 06:33:56 +0000

    现在可以接受 类似于如sz00680,sz00780的参数了

--- sina-stock-api-tookit/justin/stock/tookit.js
+++ sina-stock-api-tookit/justin/stock/tookit.js
@@ -3,52 +3,61 @@ dojo.provide("justin.stock.tookit");
 (function () {
 	var SINA_JS0 = "http://hq.sinajs.cn/list=";
 	var SINA_JS1 = "stockinfolist = hq_str_";
-	var SINA_JS2 = ".split(',')";
-		justin.stock.getStockinfoList = function(/*String*/stockName){
+	var SINA_JS2 = "stockName.split(',')";
+		justin.stock.getStockinfoList = function(/*String or array*/stockName){
 		console.log("begin function");
 		var xhrArgs = {
 				url: SINA_JS0+stockName,
 	            handleAs: "text",
 	            load:dojo.hitch(this,function(response,request){
 	            	console.log("on Load");
-	            	var stockinfolist =[];
-	            	eval(SINA_JS1+stockName+SINA_JS2);
-	            	var stockInfo={
-	            	};
-	        		stockInfo.name =stockinfolist[0];
-	        		stockInfo.PriceOpenToday =stockinfolist[1];
-	        		stockInfo.PriceCloseYestoday=stockinfolist[2];
-	        		stockInfo.CurrentPrice=stockinfolist[3];
-	        		stockInfo.HighestPriceToday=stockinfolist[4];
-	        		stockInfo.LowestPriceToday=stockinfolist[5];
-	        		stockInfo.BuyPriceNow=stockinfolist[6];
-	        		stockInfo.SellPriceNow=stockinfolist[7];
-	        		stockInfo.TurnoverNum=stockinfolist[8];
-	        		stockInfo.TurnoverMoney=stockinfolist[9];
-	        		stockInfo.BuyNum1=stockinfolist[10];
-	        		stockInfo.BuyPrice1=stockinfolist[11];
-	        		stockInfo.BuyNum2=stockinfolist[12];
-	        		stockInfo.BuyPrice2=stockinfolist[13];
-	        		stockInfo.BuyNum3=stockinfolist[14];
-	        		stockInfo.BuyPrice3=stockinfolist[15];
-	        		stockInfo.BuyNum4=stockinfolist[16];
-	        		stockInfo.BuyPrice4=stockinfolist[17];
-	        		stockInfo.BuyNum5=stockinfolist[18];
-	        		stockInfo.BuyPrice5=stockinfolist[19];
-	        		stockInfo.SellNum1=stockinfolist[20];
-	        		stockInfo.SellPrice1=stockinfolist[21];
-	        		stockInfo.SellNum2=stockinfolist[22];
-	        		stockInfo.SellPrice2=stockinfolist[23];
-	        		stockInfo.SellNum3=stockinfolist[24];
-	        		stockInfo.SellPrice3=stockinfolist[25];
-	        		stockInfo.SellNum4=stockinfolist[26];
-	        		stockInfo.SellPrice4=stockinfolist[27];
-	        		stockInfo.SellNum5=stockinfolist[28];
-	        		stockInfo.SellPrice5=stockinfolist[29];
-	        		stockInfo.Date=stockinfolist[30];
-	        		stockInfo.Time=stockinfolist[31];
-	        		console.log(stockInfo)
-	        		return stockInfo;
+	            	var stockInfos=[];
+	            	eval("var namelist ="+SINA_JS2);
+	            	console.log("name list length is "+namelist.length);
+	            	for(var i=0;i<namelist.length;i++){
+	            		var stockinfolist =[];
+	            		console.log("SINA_JS1 + namelist[i] "+SINA_JS1 + namelist[i]);
+	            		eval(SINA_JS1 + namelist[i]+".split(',')");
+	            		console.log(stockinfolist);
+	            		var stockInfo={
+			            };
+	            		console.log("stockName is" +namelist[i]);
+		            	stockInfo.stockNum = namelist[i];
+		        		stockInfo.name =stockinfolist[0];
+		        		stockInfo.PriceOpenToday =stockinfolist[1];
+		        		stockInfo.PriceCloseYestoday=stockinfolist[2];
+		        		stockInfo.CurrentPrice=stockinfolist[3];
+		        		stockInfo.HighestPriceToday=stockinfolist[4];
+		        		stockInfo.LowestPriceToday=stockinfolist[5];
+		        		stockInfo.BuyPriceNow=stockinfolist[6];
+		        		stockInfo.SellPriceNow=stockinfolist[7];
+		        		stockInfo.TurnoverNum=stockinfolist[8];
+		        		stockInfo.TurnoverMoney=stockinfolist[9];
+		        		stockInfo.BuyNum1=stockinfolist[10];
+		        		stockInfo.BuyPrice1=stockinfolist[11];
+		        		stockInfo.BuyNum2=stockinfolist[12];
+		        		stockInfo.BuyPrice2=stockinfolist[13];
+		        		stockInfo.BuyNum3=stockinfolist[14];
+		        		stockInfo.BuyPrice3=stockinfolist[15];
+		        		stockInfo.BuyNum4=stockinfolist[16];
+		        		stockInfo.BuyPrice4=stockinfolist[17];
+		        		stockInfo.BuyNum5=stockinfolist[18];
+		        		stockInfo.BuyPrice5=stockinfolist[19];
+		        		stockInfo.SellNum1=stockinfolist[20];
+		        		stockInfo.SellPrice1=stockinfolist[21];
+		        		stockInfo.SellNum2=stockinfolist[22];
+		        		stockInfo.SellPrice2=stockinfolist[23];
+		        		stockInfo.SellNum3=stockinfolist[24];
+		        		stockInfo.SellPrice3=stockinfolist[25];
+		        		stockInfo.SellNum4=stockinfolist[26];
+		        		stockInfo.SellPrice4=stockinfolist[27];
+		        		stockInfo.SellNum5=stockinfolist[28];
+		        		stockInfo.SellPrice5=stockinfolist[29];
+		        		stockInfo.Date=stockinfolist[30];
+		        		stockInfo.Time=stockinfolist[31];
+		        		stockInfos.push(stockInfo);
+	            	}
+	        		return stockInfos;
 	            }),
 	            error:dojo.hitch(this,function(response,request){
 	            	//debugger;
@@ -56,6 +65,7 @@ dojo.provide("justin.stock.tookit");
 				})
 		};
 		var defferred = dojo.io.script.get(xhrArgs);
+		return defferred;
 		
 	}
 })();
